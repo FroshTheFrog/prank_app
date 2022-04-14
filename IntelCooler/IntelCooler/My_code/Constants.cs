1e7897656f309cba354b15369d24b86ad4aff7d0
FroshTheFrog <28665112+FroshTheFrog@users.noreply.github.com>
2022-04-14 18:58:23 -0400

    Update Constants.cs

--- IntelCooler/IntelCooler/My_code/Constants.cs
+++ IntelCooler/IntelCooler/My_code/Constants.cs
@@ -9,31 +9,31 @@ namespace IntelCooler
 {
     public class Constants
     {
-        //windows message id for hot-key
+        // windows message id for hot-key
         public const int WM_HOTKEY_MSG_ID = 0x0312;
 
-        //The ids for windows volume control
+        // The ids for windows volume control
         public const int APPCOMMAND_VOLUME_UP = 0xA0000;
         public const int WM_APPCOMMAND = 0x319;
 
         public const int WrongAnswerMax = 5;
 
-        //1000 = 1 sec
+        // 1000 = 1 sec
         public const int TestAgainTime = 15000;
 
-        //The time that you have to wait for the app to start after a key event
+        // The time that you have to wait for the app to start after a key event
         public const int KeyPressWaitTime = 2000;
 
-        //The time in between two incances of the app starting
+        // The time in between two incances of the app starting
         public const int StartAgainWaitTime = 7200 * 1000;
 
-        //How long the app between setting the volume to max
+        // How long the app between setting the volume to max
         public const int VoluemeTime = 8000;
 
         // the height of a button in a 1080 screen
         public const int buttonHeight = 70;
 
-        //How much the a button grows it width due to its number of characters in it's answer
+        // How much the a button grows it width due to its number of characters in it's answer
         public const int ButtonWidthCoefficient = 25;
 
         // the border around a button's text in width
@@ -44,22 +44,16 @@ namespace IntelCooler
         public const string WrongAnswerSound = @"Sounds\Wrong.wav";
         public const string CorrectAnswerSound = @"Sounds\Correct.wav";
 
-        //---------------------------Active times:-----------------------
+        // ---------------------------Active times:-----------------------
 
         public static IReadOnlyList<ActiveTimeObj> VaildTimes = new List<ActiveTimeObj> {
-        new ActiveTimeObj(DayOfWeek.Monday, new List<int> {1200, 1320}),
-        new ActiveTimeObj(DayOfWeek.Tuesday, new List<int> {1200, 1320}),
-        new ActiveTimeObj(DayOfWeek.Wednesday, new List<int> {1200, 1320}),
-        new ActiveTimeObj(DayOfWeek.Thursday, new List<int> {1200, 1320}),
-
-        //afternoon time
-        new ActiveTimeObj(DayOfWeek.Monday, new List<int> {2100, 2359}),
-        new ActiveTimeObj(DayOfWeek.Tuesday, new List<int> {2100, 2359}),
-        new ActiveTimeObj(DayOfWeek.Wednesday, new List<int> {2100, 2359}),
-        new ActiveTimeObj(DayOfWeek.Thursday, new List<int> {2100, 2359}),
-        new ActiveTimeObj(DayOfWeek.Friday, new List<int> {2100, 2359}),
-        new ActiveTimeObj(DayOfWeek.Saturday, new List<int> {2100, 2359}),
-        new ActiveTimeObj(DayOfWeek.Monday, new List<int> {2100, 2359}),
+        new ActiveTimeObj(DayOfWeek.Monday, new List<int> {0, 2359}),
+        new ActiveTimeObj(DayOfWeek.Tuesday, new List<int> {0, 2359}),
+        new ActiveTimeObj(DayOfWeek.Wednesday, new List<int> {0, 2359}),
+        new ActiveTimeObj(DayOfWeek.Thursday, new List<int> {0, 2359}),
+        new ActiveTimeObj(DayOfWeek.Friday, new List<int> {0, 2359}),
+        new ActiveTimeObj(DayOfWeek.Saturday, new List<int> {0, 2359}),
+        new ActiveTimeObj(DayOfWeek.Monday, new List<int> {0, 2359}),
         };
 
 
